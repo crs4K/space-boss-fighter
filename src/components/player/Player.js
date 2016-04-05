b9e77ef325bf1c843151c0abc8e8de90c56baeb4
Oleh Shalyhin <cross14ek@yandex.ru>
2016-04-06 00:41:15 +0300

    refactored Player component

--- src/components/player/Player.js
+++ src/components/player/Player.js
@@ -6,37 +6,38 @@ define(["player/PlayerConstants"], function(PlayerConstants){
 
 	Player.prototype.create = function(game) {
 		this._view = game.add.sprite(PlayerConstants.X, PlayerConstants.Y, PlayerConstants.ID);
+		this._keys = game.input.keyboard.createCursorKeys();
 		
 		game.physics.enable(this._view, Phaser.Physics.ARCADE);
-		this._view.body.collideWorldBounds = true;
-		this._view.inputEnabled = true;
-		
-		this._view.speed = PlayerConstants.SPEED;
-		this._view.health = PlayerConstants.MAX_HEALTH;
-		this._view.maxHealth = PlayerConstants.MAX_HEALTH;
-
-		this._keys = game.input.keyboard.createCursorKeys();
+		this._initView();
 	};
 
 	Player.prototype.update = function() {
 		this._addMovement();
 	};
 
+	Player.prototype._initView = function() {
+		this._view.body.collideWorldBounds = true;
+		this._view.inputEnabled = true;
+		this._view.speed = PlayerConstants.SPEED;
+		this._view.health = PlayerConstants.MAX_HEALTH;
+		this._view.maxHealth = PlayerConstants.MAX_HEALTH;
+	}
+
 	Player.prototype._addMovement = function() {
+		this._view.body.velocity.x = 0;
+		this._view.body.velocity.y = 0;
+
 		if(this._keys.up.isDown) {
 			this._view.body.velocity.y = -this._view.speed;
 		} else if(this._keys.down.isDown) {
 			this._view.body.velocity.y = this._view.speed;
-		} else {
-			this._view.body.velocity.y = 0;
 		}
 
-		 if(this._keys.left.isDown) {
+		if(this._keys.left.isDown) {
 			this._view.body.velocity.x = -this._view.speed;
 		} else if(this._keys.right.isDown) {
 			this._view.body.velocity.x = this._view.speed;
-		} else {
-			this._view.body.velocity.x = 0;
 		}
 	};
 
